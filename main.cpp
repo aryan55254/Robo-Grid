413c35adca639a281dbc8ea4331592dba05b9353
aryanmishra55254 <aryanmishra55254@gmail.com>
2025-08-08 22:48:00 +0530

    small error fix

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 
+// robo class is to definig the coordinates of where robo is on the grid and functions to get its coordinates
 class robo
 {
     int x;
@@ -11,14 +12,14 @@ public:
     robo(int start_x, int start_y) : x(start_x), y(start_y) {};
     int get_x()
     {
-        return x;
+        return x - 1;
     };
     int get_y()
     {
-        return y;
+        return y - 1;
     };
 };
-
+// grid class is to define the height and width of the grid and has vector of char vector to make grid also provides public functions to display grid and place robot
 class Grid
 {
     int width;
@@ -34,15 +35,17 @@ public:
             data[i].resize(width, '.');
         }
     };
-    void place_robo(robo &robot)
+
+    void place_robot(robo &robot)
     {
         int x = robot.get_x();
         int y = robot.get_y();
-        if (y >= 0 && y < height && x >= 0 && x < width)
+        if (y >= 0 && y <= height && x >= 0 && x <= width)
         {
             data[y][x] = 'R';
         }
     };
+
     void display()
     {
         for (int i = 0; i < height; i++)
@@ -60,7 +63,7 @@ int main()
 {
     Grid grido1(10, 10);
     robo robbie(2, 4);
-    grido1.place_robo(robbie);
+    grido1.place_robot(robbie);
     grido1.display();
     return 0;
 }
