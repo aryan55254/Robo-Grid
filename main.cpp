4fc1efacb42dd7d79be526f77403421c22330402
aryanmishra55254 <aryanmishra55254@gmail.com>
2025-08-13 21:47:46 +0530

    done

--- main.cpp
+++ main.cpp
@@ -1,27 +1,63 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+#include <thread>
 
-// robo class is to definig the coordinates of where robo is on the grid and functions to get its coordinates
 class robo
 {
+private:
     int x;
     int y;
 
 public:
-    robo(int start_x, int start_y) : x(start_x), y(start_y) {};
+    robo(int start_x, int start_y) : x(start_x), y(start_y) {}
+
     int get_x()
     {
         return x;
-    };
+    }
+
     int get_y()
     {
         return y;
-    };
+    }
+
+    virtual char get_marker()
+    {
+        return 'R';
+    }
+
+    virtual void move(int grid_width, int grid_height)
+    {
+        int direction = rand() % 4;
+        switch (direction)
+        {
+        case 0: // Move Up
+            if (y > 0)
+                y--;
+            break;
+        case 1: // Move Down
+            if (y < grid_height - 1)
+                y++;
+            break;
+        case 2: // Move Left
+            if (x > 0)
+                x--;
+            break;
+        case 3: // Move Right
+            if (x < grid_width - 1)
+                x++;
+            break;
+        }
+    }
 };
-// grid class is to define the height and width of the grid and has vector of char vector to make grid also provides public functions to display grid and place robot
+
 class Grid
 {
+private:
     int width;
     int height;
     std::vector<std::vector<char>> data;
@@ -34,7 +70,17 @@ public:
         {
             data[i].resize(width, '.');
         }
-    };
+    }
+
+    int get_width()
+    {
+        return width;
+    }
+
+    int get_height()
+    {
+        return height;
+    }
 
     void place_robot(robo &robot)
     {
@@ -42,9 +88,9 @@ public:
         int y = robot.get_y();
         if (y >= 0 && y < height && x >= 0 && x < width)
         {
-            data[y][x] = 'R';
+            data[y][x] = robot.get_marker();
         }
-    };
+    }
 
     void display()
     {
@@ -62,42 +108,62 @@ public:
 class ScoutRobot : public robo
 {
 public:
-    ScoutRobot(int start_x, int start_y) : robo(start_x, start_y) {
-                                           };
+    ScoutRobot(int start_x, int start_y) : robo(start_x, start_y) {}
+
+    char get_marker() override
+    {
+        return 'S';
+    }
 };
 
 class Simulator
 {
-
+private:
     Grid grid;
     std::vector<robo *> robots;
 
 public:
     Simulator(int grid_width, int grid_height) : grid(grid_width, grid_height)
     {
-
-        robots.push_back(new ScoutRobot(0, 0));
+        robots.push_back(new robo(0, 0));
         robots.push_back(new ScoutRobot(5, 5));
     }
+
     ~Simulator()
     {
         for (robo *r : robots)
         {
             delete r;
         }
-    };
+    }
+
     void run()
     {
-        for (int i = 0; i < robots.size(); i++)
+        for (int turn = 0; turn < 15; ++turn)
         {
-            grid.place_robot(*robots[i]);
+            for (robo *r : robots)
+            {
+                r->move(grid.get_width(), grid.get_height());
+            }
+
+            grid = Grid(grid.get_width(), grid.get_height());
+
+            for (robo *r : robots)
+            {
+                grid.place_robot(*r);
+            }
+
+            std::cout << "--- Turn " << turn + 1 << " ---" << std::endl;
+            grid.display();
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
         }
-        grid.display();
-    };
+    }
 };
 
 int main()
 {
+    srand(time(0));
     Simulator sim(10, 10);
     sim.run();
     return 0;
